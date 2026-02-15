6e03cf9ce6453ed8e8e3274e1e33fdfc7ab1a801
razz.ernesto@gmail.com <ernesto@muuvment.com>
2026-02-15 20:37:18 +0800

    feat(environment): add production environment configuration and file replacements

---
+++ environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://nixgfkgozcsloysgoecz.supabase.co',
+  supabaseKey: 'sb_publishable_AP6-WhCe3R2zBcpSDh97tg_41OG_gTB',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5peGdma2dvemNzbG95c2dvZWN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxMzc5MjAsImV4cCI6MjA4NTcxMzkyMH0.MmlQH6fZaf5ZJoGMU3yBHJwgckVzX-cBwqArM079a5c',
+  jwtSecret: '8e11553ff31b80fdc2b0ff8328358a6cc85e04be14deb13a7a512280ba3570d1',
+};
